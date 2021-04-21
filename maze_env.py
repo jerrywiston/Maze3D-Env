badb046cc7cc3b2fc9a20b44384172920ca2a643
jerrywiston <dreamfantasy0@gmail.com>
2021-04-21 20:54:23 +0800

    change render view

--- maze_env.py
+++ maze_env.py
@@ -64,12 +64,22 @@ class MazeBaseEnv:
         color, depth = self._render_frame()
         # Return State / Reward / Done / Info
         return color, 0, False, {"color":color, "depth":depth, "pose":self.agent_info, "map":self.maze.get_map(self.agent_info)}
-        
+
+def _render(info, res=(192,192)):
+    color = cv2.resize(info["color"], res) 
+    depth = (cv2.resize(info["depth"]/5, res) * 255)
+    depth[depth>255] = 255
+    depth = depth.astype(np.uint8)[...,np.newaxis]
+    depth = np.concatenate([depth, depth, depth], 2)
+    map_img = cv2.resize(info["map"], res)
+    render_img = cv2.hconcat([color, depth, map_img])
+    cv2.imshow("MazeEnv", render_img)
+
 if __name__ == "__main__":
     # Select maze type.
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--type', '-t', nargs='?', type=str, default="MazeGridRoom", help='Maze type.')
+    parser.add_argument('--type', '-t', nargs='?', type=str, default="MazeBoardRandom", help='Maze type.')
     maze_type = parser.parse_args().type
     if maze_type == "MazeGridRoom":
         maze_obj = maze.MazeGridRoom()
@@ -87,17 +97,25 @@ if __name__ == "__main__":
     # Initial Env
     env = MazeBaseEnv(maze_obj, render_res=(192,192))
     state, info = env.reset()
-
-    cv2.imshow("color", state)
-    cv2.imshow("depth", info["depth"]/5)
-    cv2.imshow("map", info["map"])
+    _render(info)
 
     while(True):
         # Control Handle
-        k = cv2.waitKey(1)
+        k = cv2.waitKey(0)
         run_step = False
+        # Exit
         if k == 27:
             break
+        # Reset Maze
+        if k == 13:
+            state, info = env.reset()
+            _render(info)
+            continue
+        # Reset Pose
+        if k == 32:
+            state, info = env.reset(gen_maze=False)
+            _render(info)
+            continue
         if k == ord('w'):
             action = 0
             run_step = True
@@ -120,7 +138,5 @@ if __name__ == "__main__":
         if run_step:
             state, reward, done, info = env.step(action)
             print("\r", info["pose"], end="\t")
-            cv2.imshow("color", state)
-            cv2.imshow("depth", info["depth"]/5)
-            cv2.imshow("map", info["map"])
+            _render(info)
         
